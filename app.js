0f3f7d9f31a9125dd8d0003a04a726c518efe427
Nidoveralis <nidoveralis@gmail.com>
2022-10-21 11:32:35 +0400

    fix: errors lint

--- app.js
+++ app.js
@@ -11,9 +11,7 @@ const app = express();
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
 
-mongoose.connect('mongodb://0.0.0.0:27017/mestodb')
-  .then(() => console.log('conneted db'))
-  .catch((err) => console.log(err));
+mongoose.connect('mongodb://0.0.0.0:27017/mestodb');
 
 app.use((req, res, next) => {
   req.user = {
@@ -27,10 +25,8 @@ app.use('/users', router);
 
 app.use('/cards', routerCard);
 
-app.use('*', (req, res, next) => {
+app.use('*', (req, res) => {
   res.status(404).send({ message: 'Страница не найдена.' });
 });
 
-app.listen(PORT, () => {
-  console.log('App listening on port 3000');
-});
+app.listen(PORT);
