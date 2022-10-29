c083ed0104256a997a1db38422d2bf775668a383
Nidoveralis <nidoveralis@gmail.com>
2022-10-29 08:58:21 +0400

    feat:avatar validation

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.post('/signup',
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().min(2).max(30),
+    avatar: Joi.string().min(2).max(30).pattern(/^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   })
