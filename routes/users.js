8202e580ab3aecd7858790e9db35c4c16e47d1dd
Nidoveralis <nidoveralis@gmail.com>
2022-10-29 19:35:35 +0400

    fix

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,21 @@
 const router = require('express').Router();
 const { getUser, getUserById, editUser, editAvatar } = require('../controllers/users');
 const cookieParser = require('cookie-parser');
+const {celebrate, Joi, errors} = require('celebrate');
+const { linkValid } = require('./constants');
 
-router.use(cookieParser());
-//app.get('/posts', (req, res) => {
-  //console.log(req.cookies.jwt); // достаём токен
-//}); 
+//router.use(cookieParser());
 
 router.get('/', getUser);
 router.get('/me', getUser);
 router.get('/:userId', getUserById);
-router.patch('/me', editUser);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().min(2).max(30).pattern(linkValid),
+  })
+}) ,editUser);
 router.patch('/me/avatar', editAvatar);
 
 module.exports = router;
