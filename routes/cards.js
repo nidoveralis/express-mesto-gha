2b19ed2231fcbba7348cc337c5c4930eb2218b84
Nidoveralis <nidoveralis@gmail.com>
2022-10-30 10:40:38 +0400

    fix

--- routes/cards.js
+++ routes/cards.js
@@ -9,12 +9,20 @@ router.post('/', celebrate({
     name: Joi.string().required().min(2).max(30),
     link: Joi.string().required().min(2).max(30).pattern(linkValid)})
 }), createCard);
-router.delete('/:cardId',celebrate({
+router.delete('/:cardId', celebrate({
   headers: Joi.object().keys({
     cardId: Joi.string().required().hex(),
     })
 }), deleteCard);
-router.put('/:cardId/likes', likeCard);
-router.delete('/:cardId/likes', dislikeCard);
+router.put('/:cardId/likes', celebrate({
+  headers: Joi.object().keys({
+    cardId: Joi.string().required().hex(),
+    })
+}), likeCard);
+router.delete('/:cardId/likes', celebrate({
+  headers: Joi.object().keys({
+    cardId: Joi.string().required().hex(),
+    })
+}), dislikeCard);
 
 module.exports = router;
